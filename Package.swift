66a164f6a0eaacdf64ff1f7cff461dcad0757066
3a4oT <petro.rovenskyy@gmail.com>
2020-12-12 19:35:21 +0200

    CI imporvments

--- Package.swift
+++ Package.swift
@@ -6,8 +6,7 @@ import PackageDescription
 let package = Package(
     name: "TLIndexPathTools",
     platforms: [.macOS(.v10_15), // Catalyst
-                .iOS(.v10),
-                .tvOS(.v10)],
+                .iOS(.v10)],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
